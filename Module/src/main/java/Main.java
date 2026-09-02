527c17d6b598b931a4bd952ffcb886ecf6e59267
August <afilterm@calpoly.edu>
2026-09-02 16:42:51 -0700

    Lab 1 DOnNe

--- Module/src/main/java/Main.java
+++ Module/src/main/java/Main.java
@@ -7,6 +7,83 @@ public class Main {
     }
     public static int addSeventeen(int b) { return b + 17; }
 
+    /*
+    input: "encrypted" string
+    output: string
+    purpose: take a normal string and remove an asterik from the end and beginning of the string
+     */
+    public static String decryptorA(String encrypted) {
+        return encrypted.substring(1, (encrypted.length()-1));
+//        String decrypted = "";
+//        int e_len = encrypted.length();
+//        for (int i=1; i<(e_len-1); i++) {
+//            decrypted += encrypted.charAt(i);
+//        }
+//        return decrypted;
+    };
+
+    public static String decryptorB(String encrypted) {
+        return encrypted.substring(encrypted.length()-1) +
+                encrypted.substring(1, (encrypted.length()-1)) +
+                encrypted.substring(0, 1);
+    }
+
+    public static String decryptorC(String encrypted){
+        return encrypted.substring(encrypted.length()-3, encrypted.length()-2)+
+                encrypted.substring(0, encrypted.length()-3);
+    }
+
+    public static String decryptorD(String encrypted){
+        return encrypted.substring(0, encrypted.length()/2) +
+                encrypted.substring(encrypted.length()-1) +
+                encrypted.substring((encrypted.length()/2), encrypted.length()-1);
+    }
+
+    public static String decryptorLA(String encrypted){
+        String decrypted = "";
+        int reverse = encrypted.length()/2;
+        for (int i=0; i<encrypted.length(); i++){
+            if (i < (encrypted.length()/2)) {
+                decrypted += encrypted.charAt(reverse);
+                reverse++;
+                continue;
+            }
+            if (reverse == encrypted.length()) {
+                reverse = (encrypted.length()/2) - 1;
+            }
+            decrypted += encrypted.charAt(reverse);
+            reverse--;
+
+        }
+        return decrypted;
+    }
+
+    public static String decryptorLB(String encrypted){
+        String decrypted = "";
+        for (int i=0; i<encrypted.length(); i++) {
+            switch (encrypted.charAt(i)){
+                case 'a':
+                    decrypted += "u";
+                    break;
+                case 'e':
+                    decrypted += "a";
+                    break;
+                case 'i':
+                    decrypted += "e";
+                    break;
+                case 'o':
+                    decrypted += "i";
+                    break;
+                case 'u':
+                    decrypted += "o";
+                    break;
+                default:
+                    decrypted += encrypted.charAt(i);
+            }
+        }
+        return decrypted;
+    }
+
     static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
--- Module/src/test/java/MainTest.java
+++ Module/src/test/java/MainTest.java
@@ -11,4 +11,22 @@ class MainTest {
 
     @Test
     void addSeventeen() { assertEquals(18, Main.addSeventeen(1)); }
+
+    @Test
+    void decryptorA() { assertEquals("miasma", Main.decryptorA("*miasma*"));}
+
+    @Test
+    void decryptorB() { assertEquals("mantelpiece",Main.decryptorB("eantelpiecm"));}
+
+    @Test
+    void decryptorC() { assertEquals("largesse", Main.decryptorC("argesselay"));}
+
+    @Test
+    void decryptorD() { assertEquals("antediluvian", Main.decryptorD("antediuvianl"));}
+
+    @Test
+    void decryptorLA() { assertEquals("whisker pole", Main.decryptorLA("elop rwhiske"));}
+
+    @Test
+    void decryptorLB() { assertEquals("“Jimmy,” he said pleadingly, “I hankers fer somethin’ tuh take my mind offen the cares an’ troubles of my exactin’ duties. I craves some hard likker, the kind that falls with a clatter an’ lands with a bang. I’m plumb meloncolic, that’s what. Don’t keep me in expense no longer.", Main.decryptorLB("“Jommy,” hi seod pliedongly, “I henkirs fir sumithon’ tah teki my mond uffin thi ceris en’ truablis uf my ixecton’ datois. I crevis sumi herd lokkir, thi kond thet fells woth e clettir en’ lends woth e beng. I’m plamb milunculoc, thet’s whet. Dun’t kiip mi on ixpinsi nu lungir."));}
 }
